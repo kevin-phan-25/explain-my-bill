60b55aa2991bf2366ed3cc8ae3ec0e735b7f477e
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-12-20 15:31:17 -0500

    Update UploadBill.js

--- frontend/src/components/UploadBill.js
+++ frontend/src/components/UploadBill.js
@@ -1,1 +1,21 @@
+import { useState } from "react";
+import { explainBill } from "../api";
+
+export default function UploadBill() {
+  const [billText, setBillText] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async () => {
+    const res = await explainBill(billText);
+    setResponse(JSON.stringify(res, null, 2));
+  };
+
+  return (
+    <div>
+      <textarea value={billText} onChange={(e) => setBillText(e.target.value)} placeholder="Paste medical bill here"/>
+      <button onClick={handleSubmit}>Explain Bill</button>
+      <pre>{response}</pre>
+    </div>
+  );
+}
 

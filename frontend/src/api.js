aebb178a6afe09b8c034a568601b430131d38e58
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-12-21 05:17:39 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const WORKER_URL = "https://explainmybill.kevin-phan-25.pages.dev";
+const WORKER_URL = "https://explain-my-bill.zerotoitsuccess.workers.dev";
 
 export async function createCheckoutSession(plan) {
   const res = await fetch(`${WORKER_URL}/create-checkout-session`, {

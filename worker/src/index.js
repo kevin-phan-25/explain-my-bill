e3675304c64bfad353d71a50641437d94c3749a7
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-12-23 07:34:35 -0500

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -10,7 +10,7 @@ export default {
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Headers": "Content-Type, X-DEV-Bypass",
     };
 
     if (request.method === "OPTIONS") {

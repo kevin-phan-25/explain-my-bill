c7d38803fa3b2da0948459bfe35b1675ed8cc8ba
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-01-10 12:58:49 -0500

    Update excel.js

--- worker/src/extractors/excel.js
+++ worker/src/extractors/excel.js
@@ -1,1 +1,9 @@
+export async function processExcel(buffer) {
+  const XLSX = await import("https://cdn.jsdelivr.net/npm/xlsx@0.18.5/+esm");
+  const wb = XLSX.read(buffer, { type: "array" });
+  return wb.SheetNames.map((n, i) => ({
+    page: i + 1,
+    rawText: XLSX.utils.sheet_to_csv(wb.Sheets[n]),
+  }));
+}
 

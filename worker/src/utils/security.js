1b3d1d3acb429c1aebc6e636d3b889048cf3f8a0
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-01-10 12:57:12 -0500

    Update security.js

--- worker/src/utils/security.js
+++ worker/src/utils/security.js
@@ -1,1 +1,9 @@
+export function timingSafeEqual(a, b) {
+  const x = String(a || "");
+  const y = String(b || "");
+  if (!x || !y || x.length !== y.length) return false;
+  let out = 0;
+  for (let i = 0; i < x.length; i++) out |= x.charCodeAt(i) ^ y.charCodeAt(i);
+  return out === 0;
+}
 

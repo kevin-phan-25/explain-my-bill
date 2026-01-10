12fb438c676f79c406379b482decabdac4db1b49
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-01-10 12:57:03 -0500

    Update response.js

--- worker/src/utils/response.js
+++ worker/src/utils/response.js
@@ -1,1 +1,13 @@
+export function jsonResponse(obj, corsHeaders) {
+  return new Response(JSON.stringify(obj), {
+    headers: { "Content-Type": "application/json", ...corsHeaders },
+  });
+}
+
+export function errorResponse(msg, status, corsHeaders) {
+  return new Response(JSON.stringify({ error: msg }), {
+    status,
+    headers: { "Content-Type": "application/json", ...corsHeaders },
+  });
+}
 
